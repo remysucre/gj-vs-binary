3411d855bb97f93acaf36562eaaf64cf15e5cd5d
Remy Wang <remywang@cs.washington.edu>
2022-06-28 16:50:02 -0700

    qry wip.

--- gj/src/main.rs
+++ gj/src/main.rs
@@ -5,15 +5,76 @@ use gj::util::*;
 
 fn main() {
 
-    let company_name: Vec<CompanyName> = load_csv("../temp/company_name.csv").unwrap();
-    let company_type: Vec<CompanyType> = load_csv("../temp/company_type.csv").unwrap();
-    let info_type: Vec<InfoType> = load_csv("../temp/info_type.csv").unwrap();
-    let kind_type: Vec<KindType> = load_csv("../temp/kind_type.csv").unwrap();
-    let movie_companies: Vec<MovieCompanies> = load_csv("../temp/movie_companies.csv").unwrap();
-    let movie_info: Vec<MovieInfo> = load_csv("../temp/movie_info.csv").unwrap();
-    let movie_info_idx: Vec<MovieInfoIdx> = load_csv("../temp/movie_info_idx.csv").unwrap();
-    let title: Vec<Title> = load_csv("../temp/title.csv").unwrap();
+    let cn: Vec<CompanyName> = load_csv("../temp/cn.csv").unwrap();
+    let ct: Vec<CompanyType> = load_csv("../temp/ct.csv").unwrap();
+    let it: Vec<InfoType> = load_csv("../temp/it.csv").unwrap();
+    let it2: Vec<InfoType> = load_csv("../temp/it2.csv").unwrap();
+    let kt: Vec<KindType> = load_csv("../temp/kt.csv").unwrap();
+    let mc: Vec<MovieCompanies> = load_csv("../temp/mc.csv").unwrap();
+    let mi: Vec<MovieInfo> = load_csv("../temp/mi.csv").unwrap();
+    let miidx: Vec<MovieInfoIdx> = load_csv("../temp/miidx.csv").unwrap();
+    let t: Vec<Title> = load_csv("../temp/t.csv").unwrap();
 
-    // let mut db = Database::default();
-    // db.add_relation_with_data("company_name", 7, data)
+    let mut db = Database::default();
+    
+    let mut cn_data = vec![];
+    for t in cn {
+        cn_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("cn", 7, cn_data);
+
+    let mut ct_data = vec![];
+    for t in ct {
+        ct_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("ct", 2, ct_data);
+
+    let mut it_data = vec![];
+    for t in it {
+        it_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("it", 2, it_data);
+
+    let mut it2_data = vec![];
+    for t in it2 {
+        it2_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("it2", 2, it2_data);
+
+    let mut kt_data = vec![];
+    for t in kt {
+        kt_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("kt", 2, kt_data);
+
+    let mut mc_data = vec![];
+    for t in mc {
+        mc_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("mc", 3, mc_data);
+
+    let mut mi_data = vec![];
+    for t in mi {
+        mi_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("mi", 5, mi_data);
+
+    let mut miidx_data = vec![];
+    for t in miidx {
+        miidx_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("miidx", 3, miidx_data);
+
+    let mut t_data = vec![];
+    for t in t {
+        t_data.append(& mut t.into_values());
+    }
+    db.add_relation_with_data("t", 3, t_data);
+
+    // Variable ordering
+    // t.id = mid.movie_id = mi.movie_id = mc.movie_id
+    // t.kind_id = kt.id
+    // mi.info_type_id = it.id = mid.info_type_id
+    // mc.company_type_id = ct.id 
+    // mc.company_id = cn.id
 }
