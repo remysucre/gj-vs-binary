b36b8a1c5e2b9f18433528540be48f23611e8c18
WinstonBullen <winbullen@gmail.com>
2022-06-30 15:58:57 -0700

    Handled movie_info_idx csv import error

--- preprocessor/test.sh
+++ preprocessor/test.sh
@@ -7,7 +7,14 @@ rm temp/filters.sql
 for File in `ls ../../queries/join-order-benchmark-redux/data/${F%.*}/`
 do
     echo "DROP TABLE IF EXISTS ${File%.*};" >> temp/filters.sql
-    echo "CREATE TABLE ${File%.*} AS SELECT * FROM read_csv_auto('../../queries/join-order-benchmark-redux/data/${F%.*}/$File', header=True, delim=',', escape='\');" >> temp/filters.sql
+    if [ "$File" = "mi_idx.csv" ];
+    then
+        echo ".mode csv" >> temp/filters.sql
+        echo "CREATE TABLE mi_idx (id integer NOT NULL PRIMARY KEY, movie_id integer NOT NULL, info_type_id integer NOT NULL, info character varying NOT NULL, note character varying(1));" >> temp/filters.sql
+        echo ".import --csv --skip 1 '../../queries/join-order-benchmark-redux/data/${F%.*}/mi_idx.csv' mi_idx" >> temp/filters.sql
+    else
+        echo "CREATE TABLE ${File%.*} AS SELECT * FROM read_csv_auto('../../queries/join-order-benchmark-redux/data/${F%.*}/$File', header=True, delim=',', escape='\');" >> temp/filters.sql
+    fi
 done
 
 rm temp/join.sql
@@ -20,6 +27,6 @@ cat "../../queries/join-order-benchmark-redux/joins/$F" >> temp/join.sql
 .read 'temp/filters.sql'
 .read 'temp/join.sql'
 SELECT * FROM before EXCEPT SELECT * FROM after;
-DROP TABLE after;
-DROP TABLE before;
+DROP TABLE IF EXISTS after;
+DROP TABLE IF EXISTS before;
 EOF
