532a161a290a4c89004a137aad89f79cf7f7819b
Remy Wang <remywang@cs.washington.edu>
2022-07-12 15:52:28 -0700

    Change trie data type.

--- gj/src/join.rs
+++ gj/src/join.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 #[derive(Debug, Clone)]
 enum Trie<T> {
     Node(HashMap<usize, Self>),
-    Data(Option<Vec<T>>),
+    Data(Vec<Vec<T>>),
 }
 
 impl<T> Default for Trie<T> {
@@ -14,32 +14,99 @@ impl<T> Default for Trie<T> {
 }
 
 impl<T> Trie<T> {
-    fn insert(&mut self, ids: &[usize], data: Option<T> ) {
-        let mut trie = self;
-        for (i, id) in ids.iter().enumerate() {
-            if let Trie::Node(node) = trie {
-                trie = node.entry(*id).or_insert_with(|| {
-                    if i == ids.len() - 1 {
-                        Trie::Data(data.as_ref().map(|_| vec![]))
-                    } else {
-                        Trie::default()
-                    }
-                });
-            } else {
-                panic!("Trie::insert: tuple longer than trie depth");
-            }
-        }
-        if let Trie::Data(Some(d)) = trie {
-            if let Some(v) = data {
-                d.push(v);
-            } else {
-                panic!("Missing data");
-            }
+    fn len(&self) -> usize {
+        self.get_map().len()
+    }
+
+    fn get_map(&self) -> &HashMap<usize, Self> {
+        if let Trie::Node(ref map) = *self {
+            map
         } else {
-            assert!(data.is_none());
+            panic!("get_map() called on a Trie::Data");
         }
     }
+
+    fn get_map_mut(&mut self) -> &mut HashMap<usize, Self> {
+        if let Trie::Node(ref mut map) = *self {
+            map
+        } else {
+            panic!("get_map_mut() called on a Trie::Data");
+        }
+    }
+
+    fn get_data(&self) -> &[Vec<T>] {
+        if let Trie::Data(ref data) = *self {
+            data
+        } else {
+            panic!("get_data() called on a Trie::Node");
+        }
+    }
+
+    fn get_data_mut(&mut self) -> &mut Vec<Vec<T>> {
+        if let Trie::Data(ref mut data) = *self {
+            data
+        } else {
+            panic!("get_data_mut() called on a Trie::Node");
+        }
+    }
+
+    fn insert(&mut self, ids: &[usize], data: Vec<T> ) {
+        let mut trie = self;
+        for id in &ids[ ..ids.len()-1] {
+            trie = trie.get_map_mut().entry(*id).or_default();
+        }
+
+        let d = trie
+            .get_map_mut()
+            .entry(*ids.last().unwrap())
+            .or_insert_with(|| Trie::Data(vec![]));
+        d.get_data_mut().push(data);
+    }
 }
+
+// fn join<T, F>(
+//     relations: &[&Trie<T>], 
+//     plan: &[Vec<usize>], 
+//     select: &[Vec<usize>],
+//     f: &mut F, 
+//     tuple: &[T]
+// ) where
+//     T: Clone + Debug,
+//     F: FnMut(&[&[T]]),
+// {
+//     if plan.is_empty() {
+//         let mut payload = Vec::new();
+
+//         for relation in relations {
+//             if let Trie::Data(Some(data)) = relation {
+//                 payload.push(&data[..]);
+//             } else {
+//                 panic!("Plan is exhausted but encountered a trie node");
+//             }
+//         }
+//         f(&payload);
+//     }
+    
+//     let js = &plan[0];
+
+//     let j_min = js
+//         .iter()
+//         .copied()
+//         .min_by_key(|&j| relations[j].len())
+//         .unwrap();
+    
+//     let mut intersection: Vec<_> = if let Trie::Node(ref node) = relations[j_min] {
+//         node.keys().cloned().collect()
+//     } else {
+//         panic!("Trie node expected");
+//     };
+
+//     for &j in js {
+//         if j != j_min {
+//             let rj = &relations[j];
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -48,11 +115,11 @@ mod tests {
     #[test]
     fn trie() {
         let mut t = Trie::default();
-        t.insert(&vec![1, 2, 3], Some(vec!["hello"]));
-        t.insert(&vec![1, 2, 3], Some(vec!["world"]));
-        t.insert(&vec![1, 3, 3], Some(vec!["from"]));
-        t.insert(&vec![1, 3, 4], Some(vec!["seattle"]));
-        t.insert(&vec![1, 3, 5], Some(vec!["and", "washinton"]));
+        t.insert(&vec![1, 2, 3], vec!["hello"]);
+        t.insert(&vec![1, 2, 3], vec!["world"]);
+        t.insert(&vec![1, 3, 3], vec!["from"]);
+        t.insert(&vec![1, 3, 4], vec!["seattle"]);
+        t.insert(&vec![1, 3, 5], vec!["and", "washinton"]);
         println!("{:?}", t);
     }
 }
