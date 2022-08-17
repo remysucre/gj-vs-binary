cef215f37c2da853484a7d815b03c75fdf803812
Remy Wang <remywang@cs.washington.edu>
2022-08-16 18:36:31 -0700

    Add semi join

--- gj/src/join.rs
+++ gj/src/join.rs
@@ -1,4 +1,4 @@
-use crate::trie::*;
+use crate::{trie::*, Relation, Col};
 
 use std::fmt::Debug;
 
@@ -124,5 +124,99 @@ where
         let rels: Vec<_> = payload.iter().map(|&i| relations[i]).collect();
         let mut tuple = Vec::new();
         select(&rels, f, &mut tuple);
+    }
+}
+
+pub struct Rel<'a, T> {
+    vars: &'a [String],
+    trie: &'a Trie<T>,
+    rel: &'a mut Relation,
+    ids: &'a mut Vec<i32>,
+}
+
+pub fn semijoin(relations: &mut [&mut Rel<Value>], plan: &[Vec<usize>])
+{
+    if !plan.is_empty() {
+        let js = &plan[0];
+
+        let j_min = js
+            .iter()
+            .copied()
+            .min_by_key(|&j| relations[j].trie.get_map().unwrap().len())
+            .unwrap();
+
+        for (id, trie_min) in relations[j_min].trie.get_map().unwrap().iter() {
+            if let Some(tries) = js
+                .iter()
+                .filter(|&j| j != &j_min)
+                .map(|&j| {
+                    relations[j].trie
+                        .get_map()
+                        .unwrap()
+                        .get(id)
+                        .map(|trie| (j, trie))
+                })
+                .collect::<Option<Vec<_>>>()
+            {
+                let mut rels = relations.iter_mut().map(|r| {
+                    Rel {
+                        vars: r.vars,
+                        trie: r.trie,
+                        rel: r.rel,
+                        ids: r.ids,
+                    }
+                }).collect::<Vec<_>>();
+                
+                rels[j_min].trie = trie_min;
+                rels[j_min].ids.push(*id);
+                for (j, trie) in &tries {
+                    rels[*j].trie = trie;
+                    rels[*j].ids.push(*id);
+                }
+                let mut rs: Vec<_> = rels.iter_mut().collect();
+                semijoin(&mut rs[..], &plan[1..]);
+                rels[j_min].ids.pop();
+                for (j, _) in &tries {
+                    rels[*j].ids.pop();
+                }
+            }
+        }
+    } else {
+        reduce(relations);
+    }
+}
+
+fn reduce<'a>(relations: &mut [&'a mut Rel<Value>])
+where
+{
+    for relation in relations {
+        for (i, id) in relation.ids.iter().enumerate() {
+            let col = relation
+                .rel
+                .entry(relation.vars[i].clone())
+                .or_insert(Col::IdCol(vec![]));
+            if let Col::IdCol(v) = col {
+                v.push(*id);
+            } else {
+                unreachable!()
+            }
+        }
+        if let Trie::Data(data) = relation.trie {
+            for tuple in data {
+                for (i, v) in tuple.iter().enumerate() {
+                    let col = relation
+                        .rel
+                        .entry(relation.vars[i+relation.ids.len()].clone())
+                        .or_insert(Col::DataCol(vec![]));
+                    if let Col::DataCol(vec) = col {
+                        vec.push(v.clone());
+                    } else {
+                        unreachable!()
+                    }
+                }
+            }
+        } else {
+            unreachable!()
+        }
     }
 }
