395ab284bc69df2fc84f244e6682ac8c38d177b5
Remy Wang <remywang@cs.washington.edu>
2022-08-31 15:01:18 -0700

    WIP simplify bushy join

--- gj/src/join.rs
+++ gj/src/join.rs
@@ -268,3 +268,94 @@ fn materialize(
         }
     }
 }
+
+pub fn bj(
+    tables: &[Tb<Value>],
+    compiled_plan: &[Vec<usize>],
+    tuple: &mut Vec<Value>,
+    out: &mut Vec<Vec<Value>>,
+) {
+    let js = &compiled_plan[0];
+
+    for j_min in js {
+        if let Tb::Arr((id_cols, data_cols)) = &tables[*j_min] {
+            for i in 0..id_cols[0].len() {
+                let mut trie_min = Trie::default();
+                let ids: Vec<_> = id_cols.iter().map(|c| c[i].as_num()).collect();
+                let data: Vec<_> = data_cols.iter().map(|c| c[i].clone()).collect();
+                trie_min.insert(&ids, data);
+                let rels: Vec<_> = tables
+                    .iter()
+                    .map(|t| match &t {
+                        Tb::Arr(_) => &trie_min,
+                        Tb::Trie(trie) => trie,
+                    })
+                    .collect();
+                bj_inner(&rels, compiled_plan, tuple, out);
+            }
+            return;
+        }
+    }
+}
+
+pub fn bj_inner(
+    relations: &[&Trie<Value>],
+    compiled_plan: &[Vec<usize>],
+    tuple: &mut Vec<Value>,
+    out: &mut Vec<Vec<Value>>,
+) {
+    if compiled_plan.is_empty() {
+        mat(relations, tuple, out);
+    } else {
+        let js = &compiled_plan[0];
+
+        let j_min = js
+            .iter()
+            .copied()
+            .min_by_key(|&j| relations[j].get_map().unwrap().len())
+            .unwrap();
+
+        for (id, trie_min) in relations[j_min].get_map().unwrap().iter() {
+            if let Some(tries) = js
+                .iter()
+                .filter(|&j| j != &j_min)
+                .map(|&j| {
+                    relations[j]
+                        .get_map()
+                        .unwrap()
+                        .get(id)
+                        .map(|trie| (j, trie))
+                })
+                .collect::<Option<Vec<_>>>()
+            {
+                let mut rels = relations.to_vec();
+                rels[j_min] = trie_min;
+                for (j, trie) in tries {
+                    rels[j] = trie;
+                }
+                tuple.push(Value::Num(*id));
+                bj_inner(&rels, &compiled_plan[1..], tuple, out);
+                tuple.pop();
+            }
+        }
+    }
+}
+
+fn mat(
+    relations: &[&Trie<Value>],
+    tuple: &mut Vec<Value>,
+    out: &mut Vec<Vec<Value>>,
+) {
+    if relations.is_empty() {
+        out.push(tuple.clone());
+
+    } else if relations[0].get_data().unwrap().is_empty() {
+        mat(&relations[1..], tuple, out);
+    } else {
+        for vs in relations[0].get_data().unwrap() {
+            for v in vs { tuple.push(v.clone()); }
+            mat(&relations[1..], tuple, out);
+            for _v in vs { tuple.pop(); }
+        }
+    }
+}
